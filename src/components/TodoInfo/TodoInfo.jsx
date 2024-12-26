f9a014f5badbed4f71e8aca9b4ba4d1ba4f8c34f
AndersonAPinto <andersonaugustopinto@gmail.com>
2024-12-26 10:33:45 -0300

    solution task

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,7 +5,7 @@ export const TodoInfo = ({ todo }) => (
   <article
     className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
   >
-    <h2 className="TodoInfo__title"> {todo.title} </h2>
+    <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
